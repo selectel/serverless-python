1eea58fd50c3727bf45db28118c8b4d4f2cfc848
serovaleksandr <U74m3GQs9h57>
2021-02-04 14:56:01 +0300

    Fix problem with requirements
    Change-Id: Ia83ad00a0f3aebafed40508743e55abeca50f5b2

--- setup.py
+++ setup.py
@@ -10,13 +10,20 @@ here = path.abspath(path.dirname(__file__))
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+requires = [
+    'requests==2.24.0',
+    'keystoneauth1==3.17.1',
+    'python-keystoneclient==3.21.0'
+]
+
 setup(
     name="sels8s",
-    version="0.0.3",
+    version="1.0.2",
     author_email="sls@selectel.ru",
     description="SDK for python serverless functions",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/selectel/serverless-python",
-    packages=["sels8s"]
+    packages=["sels8s"],
+    install_requires=requires
     )
